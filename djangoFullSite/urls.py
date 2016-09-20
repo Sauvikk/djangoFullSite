3bc3b867c124ec28286114028ec577ad75d5dd88
Sauvik <sauvik123@gmail.com>
2016-09-20 23:10:22 +0530

    commit

--- djangoFullSite/urls.py
+++ djangoFullSite/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from rango import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
+    url(r'^$', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
